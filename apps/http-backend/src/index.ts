7b3beabfe5db90264697aa84dc7f059a1d9eb17e
Bhunesh Meravi <bhuneshmeravi@Bhuneshs-MacBook-Air.local>
2025-02-04 17:09:20 +0530

    create signup, signin, room EP and middleware to gate the room EP and gate the ws server using the token

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,5 +1,17 @@
 import express from "express";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
+import { middleware } from "./middleware";
 
 const app = express();
 
-app.listen(3001)
+app.use(express.json());
+
+app.post("/signup", (req, res) => {});
+app.post("/signin", (req, res) => {
+  const userId = 1;
+  const token = jwt.sign({ userId }, JWT_SECRET);
+});
+app.post("/room", middleware, (req, res) => {});
+
+app.listen(3001);
