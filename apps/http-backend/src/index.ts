dd69aee640f71a610a76e8443f866a438ace118a
Bhunesh Meravi <bhuneshmeravi@Bhuneshs-MacBook-Air.local>
2025-03-03 23:55:05 +0530

    added singup endpoint

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -2,47 +2,62 @@ import express from "express";
 import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/backend-common/config";
 import { middleware } from "./middleware";
-import { CreateUserSchema, SignInSchema, CreateRoomSchema } from "@repo/common/types"
-
+import {
+  CreateUserSchema,
+  SignInSchema,
+  CreateRoomSchema,
+} from "@repo/common/types";
+import { prismaClient } from "@repo/db/clients";
 const app = express();
 
 app.use(express.json());
 
-app.post("/signup", (req, res) => {
+app.post("/signup", async (req, res) => {
   const data = CreateUserSchema.safeParse(req.body);
-  if(!data.success){
+  if (!data.success) {
     res.json({
-      message: "Incorrect inputs"
-    })
-    return
+      message: "Incorrect inputs",
+    });
+    return;
   }
-
-  //db call
-  res.json({userId: "123"})
+  try {
+    const user = await prismaClient.user.create({
+      data: {
+        name: data.data.name,
+        email: data.data.username,
+        password: data.data.password,
+      },
+    });
+    //db call
+    res.json({ userId: user.id });
+  } catch (error) {
+    res.status(411).json({ message: "user already exist with this username" });
+  }
 });
+
 app.post("/signin", (req, res) => {
   const data = SignInSchema.safeParse(req.body);
-  if(!data.success){
+  if (!data.success) {
     res.json({
-      message: "Incorrect inputs"
-    })
-    return
+      message: "Incorrect inputs",
+    });
+    return;
   }
 
   const userId = 1;
   const token = jwt.sign({ userId }, JWT_SECRET);
-  res.json({token})
+  res.json({ token });
 });
 app.post("/room", middleware, (req, res) => {
   const data = CreateRoomSchema.safeParse(req.body);
-  if(!data.success){
+  if (!data.success) {
     res.json({
-      message: "Incorrect inputs"
-    })
-    return
+      message: "Incorrect inputs",
+    });
+    return;
   }
 
-  res.json({roomId: 123})
+  res.json({ roomId: 123 });
 });
 
 app.listen(3001);
